cfc02cefa7ec0e63f8681eee54d2709b4952305a
Rizal <43487664+rizalarifnugraha@users.noreply.github.com>
2018-10-08 15:47:13 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan31/perkenalan/Mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan31.perkenalan;
+
+/**
+ *
+ * @author acer
+ */
+public class Mahasiswa {
+
+    public String nim;
+    public String nama;
+
+    public void perkenalkanDiri(String parNim, String parNama) {
+        System.out.println("Hello Everyone");
+        System.out.println("My Nim is  : " + parNim);
+        System.out.println("My Nama is : " + parNama);
+    }
+}
